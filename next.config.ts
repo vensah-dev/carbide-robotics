8bf92eab04c89e411adff3cb46daafecb3448fca
vensah-dev <venkateshdeven14@gmail.com>
2026-05-02 12:33:32 +0800

    new nextjs.yml

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,10 @@
-import type { NextConfig } from "next";
-
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig: NextConfig = {
-  // 1. Set the sub-path for GitHub Pages
-  basePath: isProd ? '/carbide-robotics' : '',
-  
-  // 2. Fix asset loading (JS/CSS)
-  assetPrefix: isProd ? '/carbide-robotics/' : '',
-
-  // 3. Enable Static Export
-  output: 'export',
-
-  // 4. Disable image optimization (required for static hosting)
+ /**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   images: {
-    unoptimized: true,
+    // domains: ['images.unsplash.com', 'assets.aceternity.com'],
   },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
