224101664900f335f8c7ca4547c633e659951284
vensah-dev <venkateshdeven14@gmail.com>
2026-05-02 23:49:38 +0800

    used a variable this time?

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
+import icon from '@/public/icons/icon.png';
 
 export function Navbar() {
   const pathname = usePathname();
@@ -10,7 +11,7 @@ export function Navbar() {
   return (
     <nav className="w-full py-6 px-8 flex justify-between items-center border-b border-[var(--border-color)]">
       <div className="flex items-center gap-2">
-        <Image src="@/public/icons/icon.png" width={32} height={32} alt="Carbide Robotics Logo" />
+        <Image src={icon} width={32} height={32} alt="Carbide Robotics Logo" />
         <span className="text-xl font-semibold text-[var(--font-primary)]">Carbide Robotics</span>
       </div>
       <div className="flex items-center gap-8">
