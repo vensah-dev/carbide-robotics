8a650eee924fed93297f04bc95044c8fb252240a
vensah-dev <venkateshdeven14@gmail.com>
2026-05-04 16:01:34 +0800

    made interactability more subtle to differentiate from buttons

--- components/card.tsx
+++ components/card.tsx
@@ -14,7 +14,7 @@ export function Card({
   BGz?: number; 
 }) {
   return (
-    <div className={`${BGz === 1 ? "bg-background-primary" : BGz === 2 ? "bg-background-secondary" : "bg-background-tertiary"} border border-border-color/25 rounded-xl p-8 hover:brightness-95 transition-all`}>
+    <div className={`${BGz === 1 ? "bg-background-primary" : BGz === 2 ? "bg-background-secondary" : "bg-background-tertiary"} border border-border-color/25 rounded-xl p-8 hover:brightness-98 transition-all`}>
       <div className={`${colorClass} w-min rounded-2xl mb-4`}>
         <h3 className="text-2xl md:text-5xl font-bold text-font-primary p-4 w-min">{icon}</h3>
       </div>
