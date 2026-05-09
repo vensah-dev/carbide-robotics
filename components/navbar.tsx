00402261b44ff67941d0a9af40d1d54ff2de9d5e
vensah-dev <venkateshdeven14@gmail.com>
2026-05-09 21:45:35 +0800

    forgot to update import path at navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
-import icon from '@/public/icons/icon.svg';
+import icon from '@/public/icons/icon-svg.svg';
 import { useState, useEffect } from 'react';
 
 export function Navbar() {
